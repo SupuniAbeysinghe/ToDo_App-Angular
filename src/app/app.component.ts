cc7efe1eca2db069ef5c95896d4495d4e0de1fd0
SupuniAbeysinghe <supuniab1@gmail.com>
2024-08-22 17:42:53 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppTodo } from './navbar/todo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppTodo],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'first-angular-app';
 }
---
+++ src/app/navbar/todo.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+  
+
+
+})
+export class AppTodo{
+
+
+}
+
+
+ 
+        

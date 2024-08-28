396729fb3f562f9f7b3b1b53828c0e0cb12e865a
SupuniAbeysinghe <supuniab1@gmail.com>
2024-08-29 02:14:12 +0530

    implement with AXIOS

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,7 +1,6 @@
 import { Component } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-
-
+import axios from 'axios'; 
 
 @Component({
   selector: 'app-navbar',
@@ -9,43 +8,50 @@ import { FormsModule } from "@angular/forms";
   imports: [FormsModule],
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
-  
-
-
 })
-export class AppTodo{
-
-
-
-  
-
+export class AppTodo {
   todos: Array<any> = [];
 
-
-
-
-  buttonClick(todos:HTMLInputElement){
-    
-    console.log(todos.value);
-
-    let todo = {
-      id: this.todos.length + 1,
-      title: todos.value,
-      completed: false
-    };
-    this.todos.push(todo);
-    todos.value = '';
-    }
-    onDelete(todo:object){
-
-      let index = this.todos.indexOf(todo);
-      this.todos.splice(index, 1);
-    }
-
-
+  constructor() {
+    this.fetchTodos(); 
   }
 
+  fetchTodos() {
+    axios.get('http://localhost:3000/todos') 
+      .then(response => {
+        this.todos = response.data;
+      })
+  }
 
+  buttonClick(todoInput: HTMLInputElement) {
+    const todoText = todoInput.value.trim();
+    if (todoText === '') {
+      alert('You must write something!');
+      return;
+    }
 
+    const newTodo = {
+      title: todoText,
+      completed: false
+    };
 
+    axios.post('http://localhost:3000/todos', newTodo) 
+      .then(response => {
+        this.todos.push(response.data);
+        todoInput.value = ''; 
+      })
+  }
 
+  onCheckboxChange(todo: any) {
+    axios.patch(`http://localhost:3000/todos/${todo.id}`, { completed: todo.completed }) 
+  }
+
+  onDelete(todo: any) {
+    axios.delete(`http://localhost:3000/todos/${todo.id}`) 
+      .then(response => {
+        const index = this.todos.indexOf(todo);
+        this.todos.splice(index, 1); 
+        
+      })
+  }
+}
